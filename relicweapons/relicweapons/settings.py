f261df44cc254abb919ce027888956e91ea773b9
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-11-20 02:44:56 +0300

    static fix

--- relicweapons/relicweapons/settings.py
+++ relicweapons/relicweapons/settings.py
@@ -124,13 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_files')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_files')
-MEDIA_URL = '/media/'
