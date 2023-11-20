1969080a1fb8d88c7c3f1ef8f9bee2f3cc2875c5
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-11-20 15:59:20 +0300

    static

--- relicweapons/relicweapons/settings.py
+++ relicweapons/relicweapons/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_files')
 
