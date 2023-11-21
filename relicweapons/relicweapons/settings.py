c20b940e48a78e7a0c115c8bea5e79d2f7cef295
Enoth <58894013+nikitairl@users.noreply.github.com>
2023-11-21 04:05:07 +0300

    da

--- relicweapons/relicweapons/settings.py
+++ relicweapons/relicweapons/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
